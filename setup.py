56bfa63451348a35dc0b0dc3a5917790f9b0cbc1
Thomas Grainger <tagrain@gmail.com>
2019-01-31 09:56:26 +0000

    modify for pytorch

--- setup.py
+++ setup.py
@@ -2,18 +2,26 @@ import sys
 import webbrowser
 from distutils.core import setup
 
-trailer_url = 'http://youtube.com/v/rC8VJ9aeB_g?hd=1&autoplay=1'
+trailer_url = 'http://pytorch.org/#pip-install-pytorch'
+message = 'You should install pytorch from http://pytorch.org'
 
 argv = lambda x: x in sys.argv
 
 if (argv('install') or  # pip install ..
-        (argv('--dist-dir') and argv('bdist_egg'))):  # easy_install ..
+        (argv('--dist-dir') and argv('bdist_egg'))):  # easy_install
     webbrowser.open_new(trailer_url)
+    raise Exception(message)
+
+
+if argv('bdist_wheel'):  # modern pip install
+    raise Exception(message)
 
 
 setup(
-    name='django-unchained',
-    version='1.0.1',
-    maintainer='Jannis Leidel',
-    maintainer_email='jannis@leidel.info',
-    url=trailer_url)
+    name='pytorch',
+    version='0.1.2',
+    maintainer='Thomas Grainger',
+    maintainer_email='pytorch@graingert.co.uk',
+    long_description=message,
+    url=trailer_url,
+)
